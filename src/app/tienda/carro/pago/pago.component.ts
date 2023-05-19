ceae96efab6c1eaf487e04cc0f162cd7d674032f
Pixgel <pixgel13040@gmail.com>
2023-05-19 07:45:08 -0500

    pago

--- src/app/tienda/carro/pago/pago.component.ts
+++ src/app/tienda/carro/pago/pago.component.ts
@@ -11,7 +11,7 @@ export class PagoComponent implements OnInit {
   phoneInput:string=""
   addressInput: string=""
   inputDate:Date=new Date
-  inputHour:Date=new Date
+  inputHour:string="" 
 
   constructor(){
 
@@ -24,8 +24,9 @@ export class PagoComponent implements OnInit {
   // Función para obtener los valores del formulario y guardarlos en un objeto
   getFormData(): FormData {
     const newDate=new Date(this.inputDate)
-    newDate.setHours(this.inputHour.getHours())
-    newDate.setMinutes(this.inputHour.getMinutes())
+    const parts=this.inputHour.split(":")
+    newDate.setHours(+parts[0])
+    newDate.setMinutes(+parts[1])
     const formData: FormData = {
       name: this.nameInput,
       phone: this.phoneInput,
@@ -37,6 +38,8 @@ export class PagoComponent implements OnInit {
   
   // Función para enviar los datos del formulario
   submitForm() {
+    console.log("hola")
+    // event.preventDefault();
     const formData = this.getFormData();
     console.log(formData); // Aquí puedes hacer lo que quieras con los datos del formulario
   }
